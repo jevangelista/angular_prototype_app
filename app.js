2faf5a92ed1badb14f2b33fe810c2c65158aad88
Jamie Eula Evangelista <jevangelista@Jamies-MBP-2.att.net>
2015-10-18 21:14:06 -0700

    tried adding new ootd functionality. not quite working

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 var app = angular.module("myApp", ['ngResource']);
 
-app.controller("ResourceController", function($scope){
-  $scope.image = "images/jam-boho.jpg";
-  $scope.ootds = ALL_OOTDS
+app.controller("ResourceController", function($scope, OOTDService){
+  $scope.ootd = {};
+  $scope.ootds = OOTDService.query();
 
   $scope.options = ['user', 'pubdate'];
 
-
+// user can create a new ootd
+   $scope.createOOTD = function(){
+        OOTDService.save($scope.newOOTD);
+        $scope.newOOTD = {}; // clear new OOTD object
+    };
 
 });
+
+////////////
+// MODELS //
+////////////
+
+app.factory('OOTDService', function(){
+
+  var OOTDService = {};
+
+  OOTDService.query = function(){
+    return ALL_OOTDS;
+  }
+
+  OOTDService.get = function(id){
+    var id = parseInt(id);
+    return ALL_OOTDS.find(function(ootd){
+      return ootd.id == id;
+    });
+  }
+
+  return OOTDService;
+
+})
--- data.js
+++ data.js
@@ -1,111 +1,127 @@
 ALL_OOTDS = [
 	{
-		"id": "images/icon-jam.png",
+		"id": 1,
+		"icon": "images/icon-jam.png",
 		"user": "@jam",
 		"image": "images/jam1.png",
 		"title": "Print All Over Me",
 		"pubdate": new Date('2015', '09', '15')
 	},
 	{
-		"id": "images/icon-dc.png",
+		"id": 2,
+		"icon": "images/icon-dc.png",
 		"user": "@dressedchest",
 		"image": "images/chest1.png",
 		"title": "Spot the Dots",
 		"pubdate": new Date('2015', '03', '20')
 	},
 	{
-		"id": "images/icon-teeney.png",
+		"id": 3,
+		"icon": "images/icon-teeney.png",
 		"user": "@christeeney",
 		"image": "images/teeney1.png",
 		"title": "On Fleek",
 		"pubdate": new Date('2015', '10', '17')
 	},
 	{
-		"id": "images/icon-je.png",
+		"id": 4,
+		"icon": "images/icon-je.png",
 		"user": "@thejohn_ed",
 		"image": "images/john1.png",
 		"title": "Cool Blues",
 		"pubdate": new Date('2015', '05', '03')
 	},
-		{
-		"id": "images/icon-jam.png",
+	{
+		"id": 5,
+		"icon": "images/icon-jam.png",
 		"user": "@jam",
 		"image": "images/jam2.png",
 		"title": "Going Boho",
 		"pubdate": new Date('2015', '07', '22')
 	},
 	{
-		"id": "images/icon-dc.png",
+		"id": 6,
+		"icon": "images/icon-dc.png",
 		"user": "@dressedchest",
 		"image": "images/chest2.png",
 		"title": "Collegiate Leanings",
 		"pubdate": new Date('2015', '02', '04')
 	},
 	{
-		"id": "images/icon-teeney.png",
+		"id": 7,
+		"icon": "images/icon-teeney.png",
 		"user": "@christeeney",
 		"image": "images/teeney2.png",
 		"title": "Walk the Dog",
 		"pubdate": new Date('2015', '06', '12')
 	},
 	{
-		"id": "images/icon-je.png",
+		"id": 8,
+		"icon": "images/icon-je.png",
 		"user": "@thejohn_ed",
 		"image": "images/john2.png",
 		"title": "Giant Contemplation",
 		"pubdate": new Date('2015', '10', '30')
 	},
 	{
-		"id": "images/icon-jam.png",
+		"id": 9,
+		"icon": "images/icon-jam.png",
 		"user": "@jam",
 		"image": "images/jam3.png",
 		"title": "Amongst Nature",
 		"pubdate": new Date('2014', '06', '08')
 	},
 	{
-		"id": "images/icon-dc.png",
+		"id": 10,
+		"icon": "images/icon-dc.png",
 		"user": "@dressedchest",
 		"image": "images/chest3.png",
 		"title": "Fairest of the Isles",
 		"pubdate": new Date('2014', '09', '28') 
 	},
 	{	
-		"id": "images/icon-teeney.png",
+		"id": 11,
+		"icon": "images/icon-teeney.png",
 		"user": "@christeeney",
 		"image": "images/teeney3.png",
 		"title": "A Bed of Roses",
 		"pubdate": new Date('2014', '02', '22') 
 	},
 	{
-		"id": "images/icon-jam.png",
+		"id": 12,
+		"icon": "images/icon-jam.png",
 		"user": "@jam",
 		"image": "images/jam4.png",
 		"title": "Spring Palette",
 		"pubdate": new Date('2015', '04', '12')
 	},
 	{	
-		"id": "images/icon-je.png",
+		"id": 13,
+		"icon": "images/icon-je.png",
 		"user": "@thejohn_ed",
 		"image": "images/john3.png",
 		"title": "Street Wears",
 		"pubdate": new Date('2014', '11', '11') 
 	},
 	{
-		"id": "images/icon-dc.png",
+		"id": 14,
+		"icon": "images/icon-dc.png",
 		"user": "@dressedchest",
 		"image": "images/chest4.png",
 		"title": "Shawl Collar",
 		"pubdate": new Date('2014', '08', '10') 
 	},
 	{
-		"id": "images/icon-teeney.png",
+		"id": 15,
+		"icon": "images/icon-teeney.png",
 		"user": "@christeeney",
 		"image": "images/teeney4.png",
 		"title": "Twirl Around the World",
 		"pubdate": new Date('2015', '01', '10') 
 	},
 	{
-		"id": "images/icon-je.png",
+		"id": 16,
+		"icon": "images/icon-je.png",
 		"user": "@thejohn_ed",
 		"image": "images/john4.png",
 		"title": "All American",
